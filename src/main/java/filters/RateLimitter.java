e2f537536efae8fe4f6a22c1f4038572f4e50f76
Troy Sellers <tsellers@salesforce.com>
2018-08-18 07:52:49 +1000

    fix

--- src/main/java/filters/RateLimitter.java
+++ src/main/java/filters/RateLimitter.java
@@ -10,6 +10,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -20,6 +21,10 @@ import org.slf4j.LoggerFactory;
 import io.github.bucket4j.*;
 import utils.SecurityUtils;
 
+@WebServlet(
+		name="RateLimiter",
+		urlPatterns="/servlet/*"
+)
 public class RateLimitter implements Filter {
 
 	private Logger log;
